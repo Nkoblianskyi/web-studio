c270650eeb62f0426515fecacb4b7e2df2a6056d
Nykyta Koblianskyi <nkoblianskyi@gmail.com>
2025-04-06 12:33:13 +0200

    hero

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/web-studio',
+  assetPrefix: '/web-studio',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
